bb61597c5e751d71ae2ad1977e7d8333c9bf22dd
VickyPal2022 <ftd10622582@dseu.ac.in>
2025-02-12 10:03:13 +0530

    fix route

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensure proper handling of routes in dev mode
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
